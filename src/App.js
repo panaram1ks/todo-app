eb0b68007bd0181a42078baa7167f1d7cfb77ff8
panaram1ks <parom.evgen@mail.ru>
2023-04-07 22:42:03 +0300

    import Component, function Component, class Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-        My Todo Application
+        <FirtsComponent></FirtsComponent>
+        <SecondComponent></SecondComponent>
+        <ThirdComponent></ThirdComponent>
+        <FourthComponent></FourthComponent>
     </div>
   );
+}
+
+function FirtsComponent(){
+  return (
+    <div className="FirstComponent">FirstComponent</div>
+  )
+}
+
+function SecondComponent(){
+  return (
+    <div className="SecondComponent">SecondComponent</div>
+  )
+}
+
+class ThirdComponent extends Component {
+  render (){
+    return (
+      <div className="ThirdComponent">ThirdComponent</div>
+    )
+  }
+}
+
+class FourthComponent extends Component{
+  render(){
+    return (
+      <div className="FourthComponent">FourthComponent</div>
+    )
+  }
 }
 
 export default App;
