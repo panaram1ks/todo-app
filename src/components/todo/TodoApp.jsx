35c40ceecd2a32980a27b89c1bd6237b1f87be8d
panaram1ks <parom.evgen@mail.ru>
2023-04-09 08:38:41 +0300

    update view element depends on state

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -14,18 +14,59 @@ function LoginComponent(){
 
     const [username, setUsername] = useState('in28minutes')
     const [password, setPassword] = useState('dummy')
+    const [showSuccessMessage, setshowSuccessMessage] = useState(false)
+    const [showErrorMessage, setshowErrorMessage] = useState(false)
 
     function handleUsernameChange(event){
         setUsername(event.target.value)
     }
 
     function handlePasswordChage(event){
-        console.log(event.target.value);
-        setPassword(event.target.value)
+       {/* console.log(event.target.value);*/}
+        setPassword(event.target.value)     
+    }
+
+    function handleSubmit(){
+        console.log(username);
+        console.log(password);
+        if(username==='in28minutes' && password==='dummy'){
+            console.log('Success');
+            setshowSuccessMessage(true)
+            setshowErrorMessage(false)
+        } else {
+            console.log('Authentification');
+            setshowSuccessMessage(false)
+            setshowErrorMessage(true)
+        }
+    }
+
+    function SuccessMessageComponent() {
+        if(showSuccessMessage){
+            return (
+                <div className='successMessage'>Authenticated Successfully</div>
+            )
+        } else {
+            return null
+        }
+        
+    }
+
+    function ErrorMessageComponent() {
+        if(showErrorMessage){
+            return (
+                <div className='errorMessage'>Authentication Failed. Please check your credentials.</div>
+            )
+        } else {
+            return null
+        }
+        
     }
 
     return(
         <div className="Login">
+            <SuccessMessageComponent/>
+            <ErrorMessageComponent/>
+
             <div className="LoginForm">
                 <div>
                     <label>Username:</label>
@@ -37,13 +78,14 @@ function LoginComponent(){
                 </div>
             </div>
             <div>
-                <button type="button" name="login">login</button>
+                <button type="button" name="login" onClick={handleSubmit}>login</button>
             </div>
         </div>
     )
 }
 
-function WelcomeComponent(){
+
+function WelcomeComponent() {
     return(
         <div className="Welcome">
             WelcomeComponent
