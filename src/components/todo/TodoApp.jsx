41671f440d4c86ba8d35b7e5d9f056912152cf86
panaram1ks <parom.evgen@mail.ru>
2023-04-09 09:12:23 +0300

    error page routing

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -10,6 +10,7 @@ export default function TodoApp(){
                     <Route path='/' element={<LoginComponent/>}></Route>
                     <Route path='/login' element={<LoginComponent/>}></Route>
                     <Route path='/welcome' element={<WelcomeComponent/>}></Route>
+                    <Route path='*' element={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>            
         </div>
@@ -72,6 +73,7 @@ function LoginComponent(){
 
     return(
         <div className="Login">
+            <h1>Time to Login</h1>
             {showSuccessMessage && <div className='successMessage'>Authenticated Successfully</div>}
             {showErrorMessage && <div className='errorMessage'>Authentication Failed. Please check your credentials.</div>}
             {/*<SuccessMessageComponent/>
@@ -97,8 +99,20 @@ function LoginComponent(){
 
 function WelcomeComponent() {
     return(
-        <div className="Welcome">
-            WelcomeComponent
+        <div className="WelcomeComponent">
+            <h1>Welcom to in28minutes</h1>
+            <div>
+                WelcomeComponent
+            </div>
+        </div>
+    )
+}
+
+function ErrorComponent() {
+    return(
+        <div className="ErrorComponent">
+            <h1>We are working really hard!</h1>
+            <div>Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.</div>
         </div>
     )
 }
