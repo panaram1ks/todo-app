69108c03a4cb462fcad6d8d2b0abc7d8bf5fdb4d
panaram1ks <parom.evgen@mail.ru>
2023-04-14 23:01:19 +0300

    install axios, axios use Promise, problem with CORS

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,4 +1,5 @@
 import { useParams,Link } from 'react-router-dom'
+import axios from 'axios';
 
 function WelcomeComponent() {
 
@@ -7,11 +8,31 @@ function WelcomeComponent() {
     const {username} = useParams()
     console.log(username);
 
+    function callHelloWorldRestApi(){
+        console.log('called');
+        //axios
+        axios.get('http://localhost:8080/hello-world')
+        .then( (response)=> successfulResponse(response) )
+        .catch( (error) => errorResponse(error) )
+        .finally( () => console.log('cleanup') )
+    }
+
+    function successfulResponse(response){
+        console.log(response);
+    }
+
+    function errorResponse(error){
+        console.log(error);
+    }
+
     return(
         <div className="WelcomeComponent">
             <h1>Welcom {username}</h1>
             <div>
                 Manage your todos. <Link to="/todos">Go to</Link>
+            </div>
+            <div>
+                <button className='btn btn-success m-5' onClick={callHelloWorldRestApi}>Call Hellow World</button>
             </div>
         </div>
     )
