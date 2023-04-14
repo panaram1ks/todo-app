e01091fb7cf24a3e715902e015074f2c5cf28f25
panaram1ks <parom.evgen@mail.ru>
2023-04-14 23:24:45 +0300

    show message from rest service to front page use cors

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,24 +1,36 @@
 import { useParams,Link } from 'react-router-dom'
 import axios from 'axios';
+import { useState } from 'react';
 
 function WelcomeComponent() {
 
     /*const params = useParams()
     console.log(params.username);*/
     const {username} = useParams()
-    console.log(username);
+    //console.log(username);
+
+    const [message, setMessage] = useState(null)
+
 
     function callHelloWorldRestApi(){
         console.log('called');
         //axios
-        axios.get('http://localhost:8080/hello-world')
-        .then( (response)=> successfulResponse(response) )
-        .catch( (error) => errorResponse(error) )
-        .finally( () => console.log('cleanup') )
+     /*  axios.get('http://localhost:8080/hello-world')
+            .then( (response)=> successfulResponse(response) )
+            .catch( (error) => errorResponse(error) )
+            .finally( () => console.log('cleanup') )
+      */      
+
+        axios.get('http://localhost:8080/hello-world-bean')
+            .then( (response)=> successfulResponse(response) )
+            .catch( (error) => errorResponse(error) )
+            .finally( () => console.log('cleanup') )
     }
 
     function successfulResponse(response){
         console.log(response);
+        /*setMessage(response.data)*/
+        setMessage(response.data.message)
     }
 
     function errorResponse(error){
@@ -34,6 +46,7 @@ function WelcomeComponent() {
             <div>
                 <button className='btn btn-success m-5' onClick={callHelloWorldRestApi}>Call Hellow World</button>
             </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
