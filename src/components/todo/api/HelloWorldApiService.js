a63c16250e60de32ba3b1fd52badffffb4e72386
panaram1ks <parom.evgen@mail.ru>
2023-04-15 17:35:20 +0300

    add headers for security

--- src/components/todo/api/HelloWorldApiService.js
+++ src/components/todo/api/HelloWorldApiService.js
@@ -12,4 +12,10 @@ const apiClient = axios.create(
 
 export const retrieveHelloWorldBean = () => apiClient.get('/hello-world-bean')
 
-export const retrieveHelloWorldPathVariable = (username) => apiClient.get(`/hello-world/path-variable/${username}`)
+export const retrieveHelloWorldPathVariable = (username) => apiClient.get(
+    `/hello-world/path-variable/${username}`,
+    {
+        headers: {
+            Authorization: 'Basic aW4yOG1pbnV0ZXM6ZHVtbXk='
+        }   
+    })
